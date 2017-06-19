37e33162047e0fc7128a9ea191a425511ff1015d
Darshan S. Gaonkar <DarshanG@ESTUATE.COM>
2017-06-19 16:14:40 +0530

    Added a listView to Show hardcoded data in Widget

---
+++ app/src/main/java/singh/durgesh/com/applocker/Widget/WidgetProvider.java
@@ -0,0 +1,38 @@
+package singh.durgesh.com.applocker.Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import singh.durgesh.com.applocker.R;
+
+/**
+ * Created by DarshanG on 6/19/2017.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context ctxt, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            Intent intent = new Intent(ctxt, WidgetService.class);
+
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            RemoteViews widget = new RemoteViews(ctxt.getPackageName(),
+                    R.layout.widget);
+
+            widget.setRemoteAdapter(appWidgetIds[i], R.id.words,
+                    intent);
+
+
+            appWidgetManager.updateAppWidget(appWidgetIds[i], widget);
+        }
+
+        super.onUpdate(ctxt, appWidgetManager, appWidgetIds);
+    }
+}
---
+++ app/src/main/java/singh/durgesh/com/applocker/Widget/WidgetService.java
@@ -0,0 +1,15 @@
+/**
+ * Created by DarshanG on 6/19/2017.
+ */
+package singh.durgesh.com.applocker.Widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+  @Override
+  public RemoteViewsFactory onGetViewFactory(Intent intent) {
+    return(new BlockedAppsViewsFactory(this.getApplicationContext(),
+                                 intent));
+  }
+}
