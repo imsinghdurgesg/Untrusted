6ea9174ade3791f1e75e1d55ac7965ed10e1df6b
Darshan S. Gaonkar <DarshanG@ESTUATE.COM>
2017-07-20 18:17:43 +0530

    Added Animation in the viewpager

--- app/src/main/java/app/untrusted/adapter/ViewPagerAdapter.java
+++ app/src/main/java/app/untrusted/adapter/ViewPagerAdapter.java
@@ -1,12 +1,15 @@
 package app.untrusted.adapter;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -22,51 +25,92 @@ public class ViewPagerAdapter extends PagerAdapter {
 
     private Context mContext;
 
+    View mApplock;
+
+    View mCalllock;
+
     public ViewPagerAdapter(Context context) {
         mContext = context;
     }
 
     @Override
     public Object instantiateItem(ViewGroup collection, int position) {
-
         View view = new View(collection.getContext());
-
         LayoutInflater inflater = (LayoutInflater) collection.getContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        int resId = 0;
-        switch (position) {
-            case 0:
-                resId = R.layout.view_first;
-                view = inflater.inflate(R.layout.view_first, null, false);
-                RelativeLayout linearLayout = (RelativeLayout) view.findViewById(R.id.linear_layout);
-                Button button = (Button) linearLayout.findViewById(R.id.button);
-                button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent  intent = new Intent(mContext,SplashScreenActivity.class);
-                        mContext.startActivity(intent);
-                        ((LaunchScreenViewPagerActivity)mContext).finish();
-                    }
-                });
-                break;
+        if (position == 0) {
+            view = inflater.inflate(R.layout.view_first, null, false);
+            RelativeLayout linearLayout = (RelativeLayout) view.findViewById(R.id.linear_layout);
+            mApplock = linearLayout.findViewById(R.id.rocket);
+                      startAnimation();
+                      linearLayout.setOnClickListener(new View.OnClickListener() {
+                          @Override
+                          public void onClick(View v) {
+                              startAnimation();
 
-            case 1:
-                resId = R.layout.view_second;
-                view = inflater.inflate(R.layout.view_second, null, false);
-                RelativeLayout linearLayoutThird = (RelativeLayout) view.findViewById(R.id.linear_layout2);
-                Button buttonEnterApp = (Button) linearLayoutThird.findViewById(R.id.buttonEnterApp);
-                buttonEnterApp.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent  intent = new Intent(mContext,SplashScreenActivity.class);
-                        mContext.startActivity(intent);
-                        ((LaunchScreenViewPagerActivity)mContext).finish();
-                    }
-                });
-                break;
-                  }
+                          }
+                      });
+            Button button = (Button) linearLayout.findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(mContext, SplashScreenActivity.class);
+                    mContext.startActivity(intent);
+                    ((LaunchScreenViewPagerActivity) mContext).finish();
+                }
+            });
+        } else {
+            view = inflater.inflate(R.layout.view_second, null, false);
+            RelativeLayout linearLayoutThird = (RelativeLayout) view.findViewById(R.id.linear_layout2);
+            mCalllock = linearLayoutThird.findViewById(R.id.rocket);
+            startCallAnimation();
+            linearLayoutThird.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startCallAnimation();
+                }
+            });
+            Button buttonEnterApp = (Button) linearLayoutThird.findViewById(R.id.buttonEnterApp);
+            buttonEnterApp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(mContext, SplashScreenActivity.class);
+                    mContext.startActivity(intent);
+                    ((LaunchScreenViewPagerActivity) mContext).finish();
+                }
+            });
+        }
         ((ViewPager) collection).addView(view, 0);
         return view;
+    }
+
+    private void startCallAnimation() {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 800);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                mCalllock.setTranslationX(value);
+            }
+        });
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setDuration(2500L);
+        valueAnimator.start();
+
+    }
+
+    private void startAnimation() {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 800);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                mApplock.setTranslationX(value);
+            }
+        });
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setDuration(2500L);
+        valueAnimator.start();
     }
 
     @Override
